44b96f003a0226d98cd2c7a9fcfc6b203b22f0d8
mcartyn <54342139+mcartyn@users.noreply.github.com>
2022-05-12 20:16:46 -0700

    python project commit - May 12wq

--- my_pyapp_api/models.py
+++ my_pyapp_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    uuid = models.UUIDField(primary_key=True)
+    price = models.PositiveIntegerField
+    discount = models.FloatField
+    tax = models.FloatField
+    order_id = models.UUIDField
+
+
+class Order(models.Model):
+    uuid = models.UUIDField
+    total_amount = models.PositiveBigIntegerField
+    discount_amount = models.IntegerField
+    tax_amount = models.IntegerField
+    items = models.ForeignKey(to_field=Item.order_id)
+
--- my_pyapp_project/urls.py
+++ my_pyapp_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from my_pyapp_api import views
 
 urlpatterns = [
+    path('', views.welcomePage, name='index'),
     path('admin/', admin.site.urls),
+    path('hello/', views.welcomePage, name='index'),
 ]
